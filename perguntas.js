02d0d55154bdf1ba206ff47d0d871af7d1e8169a
Professora Carolina Assis <163206590+ProfCarolAssis@users.noreply.github.com>
2025-05-16 15:22:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Caminhando ao fim da tarde, uma senhora contou 20 casas em uma rua à sua direita. No regresso, ela contou 20 casas à sua esquerda. Quantas casas ela viu no total?',
+    'A senhora viu 20 casas no total, porque a sua direita na ida é a sua esquerda na volta, ou seja, nos dois itinerários, ela viu e contou as mesmas casas, e não casas diferentes.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que o filho do matemático fala quando quer ir ao banheiro?',
+    'Pi-pi.'
+)
+
+criaCartao(
+    'Matemática',
+    'Há um pato entre dois patos, um pato atrás de um pato e um pato na frente de outro pato. De quantos patos estamos falando?',
+    '3 patos.'
+)
+
+criaCartao(
+    'Matemática',
+    'No meu jardim existe 3 pés de alface, 1 de pepino e 5 de cenoura. Quantos pés eu tenho no total?',
+    'Eu, como a maior parte das pessoas, tenho 2 pés.'
+)
